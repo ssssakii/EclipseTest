990534c3b734f58861bdc3ad00641d1c3b8bbfdc
g15948ts <g15948ts@s30423.tsuda.ac.jp>
2017-04-21 15:44:18 +0900

    First commit

---
+++ src/testHelloWorld2/HelloWorld.java
@@ -0,0 +1,10 @@
+package testHelloWorld2;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello!!!!");
+    }
+
+}
